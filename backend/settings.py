9734b323df077a9ead8aec7bfe420acb3e2e95f4
hariharan1476 <haranh503@gmail.com>
2025-04-06 07:01:30 +0530

    ready to deploy

--- backend/settings.py
+++ backend/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e#y4u@&nits%u4=#&(i^&tfrvu$=kdv^y7*iqx3&^sx1i)vrd_'
-
+SECRET_KEY = "django-insecure-e#y4u@&nits%u4=#&(i^&tfrvu$=kdv^y7*iqx3&^sx1i)vrd_"
+DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = []
@@ -47,10 +47,6 @@ TEMPLATES = [
 
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'tasks/static']
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
