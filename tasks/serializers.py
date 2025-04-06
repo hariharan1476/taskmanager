fbb4c20dc438437933f892524b373748816fc7be
hariharan1476 <haranh503@gmail.com>
2025-04-06 14:29:25 +0530

    hariii Final

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,12 @@
-# tasks/serializers.py
-
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=Task._meta.get_field('user').remote_field.model.objects.all(),
+        label="Admin"  # This changes the display name in the DRF UI
+    )
+
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['title', 'description', 'due_date', 'completed', 'is_completed', 'user']  # user (Admin) at last
